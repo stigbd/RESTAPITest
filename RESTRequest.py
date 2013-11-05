0fa2c1938a8d03315592804a00bc73b85353231e
Stig B. Dørmænen <stigbd@gmail.com>
2013-11-05 15:10:00 +0100

    Started on an example of using requests to make http-calls to target

--- RESTRequest.py
+++ RESTRequest.py
@@ -1,1 +1,9 @@
 # -*- coding: utf-8 -*-
+import requests
+
+
+def get_data():
+    # TODO: get headers and query-parameters as parameters
+    # feks headers: sjekk
+    # http://docs.python-requests.org/en/latest/user/quickstart/#passing-parameters-in-urls
+    return requests.get('http://example.com').content
